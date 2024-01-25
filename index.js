d8c6c5823d8d0e368cc1e33a75dd1ab9809b8063
paulina koch <pjassakoch@gmail.com>
2024-01-25 09:55:51 -0500

    Adding index.js to api routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./routes/api');
 
 const cwd = process.cwd();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
 });
